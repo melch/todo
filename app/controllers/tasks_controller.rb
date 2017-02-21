9884711b6a3c01398e956f87fb6c5e4ba8037ff2
melch <melch@procore.com>
2017-02-20 21:48:02 -0800

    Create tasks controller and views: show, index, new, delete

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,34 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all.order("created_at DESC").limit(10)
+  end
+
+  def new
+  end
+
+  def create
+    @task = Task.new(article_params)
+
+    if @task.save
+      redirect_to :tasks
+    else
+      render plain: params.inspect
+    end
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+  def article_params
+    params.require(:task).permit(:name, :description, :completed)
+  end
+end
