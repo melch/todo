f7564de680af2a2aa499f67c276a36aa872d76e6
melch <melch@procore.com>
2017-03-05 20:08:17 -0800

    Fix show completed filter.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   def index
-    @show_completed = !params[:show_completed].nil?
-    @tasks = Task.display_list.limit(20)
+    show_completed = !params[:show_completed].nil?
+    @tasks = Task.display_list(show_completed).limit(20)
   end
 
   def new
